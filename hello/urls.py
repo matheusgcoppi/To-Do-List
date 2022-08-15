b6fa704bf8bd9f7bd983369618f884a1b5bd47a4
matheusgcoppi <matheuscoppi22@gmail.com>
2022-08-15 18:08:17 -0300

    update

--- hello/urls.py
+++ hello/urls.py
@@ -1,8 +1,9 @@
-
-from importlib.resources import path
-from unittest.mock import patch
-
 from django.urls import path
+
+from . import views # from the current directory import views
+
 urlpatterns = [
-    path("", views.index, name='index')
+    path("", views.index, name='index'), # when you type /hello it will show up because of the empty string
+    path("matheus", views.matheus, name='matheus'),
+    path("<str:name>", views.greeting, name="greeting") #any string after slash that I type will activate the function
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,20 @@
-from http.client import HTTPResponse
-from urllib import request
+from http.client import HTTP_PORT
+import re
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse('hello world');
+    return render(request, "hello/index.html")
+
+def matheus(request):
+    return HttpResponse("Hello, Matheus")
+
+# def greet(request, name):
+#   return HttpResponse(f'Hello, {name.capitalize()}!')   .capitalize is for put the first letter of name in capital 
+
+def greeting(request, name):
+    return render(request, "hello/greeting.html", {
+        "name": name.capitalize()
+    })
+    

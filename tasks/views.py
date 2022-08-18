85dcd97a097c0709d02d7727b8c286efa9877018
matheusgcoppi <matheuscoppi22@gmail.com>
2022-08-18 19:41:27 -0300

    start new app

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
 import datetime
+
+# Create your views here.
 
 def index(request):
     now = datetime.datetime.now()
-    return render(request, "newyear/index.html", {
+    return render(request, "tasks/index.html", {
         'newyear': now.day == 18 and now.month == 8 
     })
